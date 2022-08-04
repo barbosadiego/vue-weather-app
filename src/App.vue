3e1a4ce631b8a80677bbc0e21d1d856511a5ddc7
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-04 14:53:17 -0300

    app background

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="overlay"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+#app{
+  width: 100%;
+  height: 100vh;
+  background-image: url(./assets/background.jpg);
+  background-size: cover;
+  background-position: center bottom;
+  display: grid;
+}
+.overlay{
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, .5);
+  grid-column: 1;
 }
 </style>
