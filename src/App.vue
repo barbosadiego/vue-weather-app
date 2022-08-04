78aba3f1b8c9d2c109e7a77fe184f380224c14f0
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-04 16:18:46 -0300

    api fetch 1

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
   <div id="app">
     <div class="overlay"></div>
     <main>
-      <div class="search">
-        <input
+      <div>
+        <input 
+          class="search"
           type="text"
           placeholder="Type here and press Enter"
           v-model="location"
+          @keydown.enter="getWeather"
         />
       </div>
 
-      <div class="info">
-        <h1>Recife, PE</h1>
+      <div class="info" v-if="responseData">
+        
+        <h1>{{responseData.name}}, {{responseData.sys.country}}</h1>
         <p>Quinta, 04 de agosto de 2022</p>
-        <p class="temp">21ªC</p>
-        <p class="description">Clouds</p>
+        <!-- <div class="temp">22 ºC</div> -->
+        <p class="temp">{{Math.round(responseData.main.temp)}}ºC</p>
+        <p class="description">{{responseData.weather[0].description}}</p>
       </div>
     </main>
   </div>
@@ -26,8 +30,20 @@ export default {
   data() {
     return {
       location: '',
+      api: 'eb9f08d66580680f98b067338772bcb0',
+      responseData: null,
     };
   },
+  methods:{
+    getWeather(){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${this.api}&units=metric&lang=pt_br`)
+        .then(res => res.json())
+        .then(json => {
+          this.responseData = json
+          console.log(json)
+        })
+    }
+  }
 };
 </script>
 
@@ -62,5 +78,31 @@ main {
   width: 100%;
   height: 100vh;
   padding: 5rem;
+}
+.search{
+  padding: 10px 15px;
+  width: 300px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1rem;
+  border-radius: 15px 0px 15px 0px;
+  border: none;
+  outline: none;
+  text-align: center;
+  margin-bottom: 50px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+.info{
+  color: white;
+}
+.temp{
+  font-size: 5rem;
+  font-weight: 900;
+  text-shadow: 4px 6px rgba(0, 0, 0, 0.2);
+  background-color: rgba(255, 255, 255, .2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  border-radius: 10px;
 }
 </style>
